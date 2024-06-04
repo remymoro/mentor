4b3fbc9777b17ca9e1cdf7e5588dc0e26d0536d9
remymoro <ubuntu@vps-f436652a.vps.ovh.net>
2024-06-04 07:22:08 +0000

    Implemented basic CRUD operations for subjects dummy

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { InterfaceSubject } from './subject';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { InterfacePostSubject, InterfaceSubject } from './subject';
 import { SUBJECTS } from './bdd';
+import { SubjectService } from './subject.service';
 
 @Controller('subject')
 export class SubjectController {
-
-  @Post()
-  addSubject(@Body() subject: InterfaceSubject): InterfaceSubject[] {
-    return [...SUBJECTS, subject];
-  }
+  constructor(private readonly subjectService: SubjectService) {}
 
 
   @Get()
   findAll(): InterfaceSubject[] {
     return SUBJECTS;
   }
+
+  @Get(':id')
+  findOneById(@Param('id') id: string): InterfaceSubject {
+    return this.subjectService.findOneById(+id);
+  }
+
+  @Post()
+  addSubject(@Body() subject: InterfacePostSubject): InterfaceSubject[] {
+    return this.subjectService.createNewSubject(subject);
+  }
+ 
 }
